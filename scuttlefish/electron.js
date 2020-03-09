618872945dfd81e9dc9173126dfe173b2c846756
Vincent Ahrend <mail@vincentahrend.com>
2020-03-09 10:19:51 +0100

    Update electron script

--- scuttlefish/electron.js
+++ scuttlefish/electron.js
@@ -1,31 +1,53 @@
-const { app, BrowserWindow, protocol } = require("electron")
-const path = require("path")
-const os = require("os")
+process.on("uncaughtException", function(err) {
+  console.log(err);
+  process.exit();
+});
 
-let mainWindow = null
-app.allowRendererProcessReuse = true
+const { app, BrowserWindow, protocol } = require("electron");
+const WindowState = require("electron-window-state");
+const path = require("path");
+const os = require("os");
+
+let mainWindow = null;
+app.allowRendererProcessReuse = true;
 
 const createWindow = async () => {
+  const windowState = WindowState({
+    defaultWidth: 1200,
+    defaultHeight: 800
+  });
+
   mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    minWidth: 800,
+    x: windowState.x,
+    y: windowState.y,
+    width: windowState.width,
+    height: windowState.height,
+    titleBarStyle: "hiddenInset",
+    autoHideMenuBar: true,
+    title: "Scuttlefish",
+    show: true,
+    backgroundColor: "#EEE",
     webPreferences: {
       nodeIntegration: true
     }
-  })
+  });
+
+  windowState.manage(mainWindow);
+  mainWindow.setSheetOffset(40);
 
   // mainWindow.loadFile(path.join(__dirname, "dist/index.html"))
-  mainWindow.loadURL("http://localhost:8080")
+  mainWindow.loadURL("http://localhost:8080");
 
   mainWindow.on("closed", () => {
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 
   mainWindow.on("ready-to-show", () => {
-    mainWindow.show()
-    mainWindow.focus()
-  })
-}
+    mainWindow.show();
+    mainWindow.focus();
+  });
+};
 
 app.on("ready", () => {
   BrowserWindow.addDevToolsExtension(
@@ -33,26 +55,27 @@ app.on("ready", () => {
       os.homedir(),
       "/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.5.0_0/"
     )
-  )
+  );
 
   protocol.interceptFileProtocol(
     "file",
     (request, callback) => {
-      const url = request.url.substr(7) /* all urls start with 'file://' */
-      callback({ path: path.normalize(`${__dirname}/${url}`) })
+      const url = request.url.substr(7); /* all urls start with 'file://' */
+      // eslint-disable-next-line standard/no-callback-literal
+      callback({ path: path.normalize(`${__dirname}/${url}`) });
     },
     err => {
-      if (err) console.error("Failed to register protocol")
+      if (err) console.error("Failed to register protocol");
     }
-  )
+  );
 
-  createWindow()
-})
+  createWindow();
+});
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit()
-})
+  if (process.platform !== "darwin") app.quit();
+});
 
 app.on("activate", () => {
-  if (mainWindow === null) createWindow()
-})
+  if (mainWindow === null) createWindow();
+});
